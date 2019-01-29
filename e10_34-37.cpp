a5606ab9d1d930c805099c0fee7c951b16254b01
Changro Yoon <cryoon@deviceeng.co.kr>
2019-01-29 17:05:10 +0900

    update

--- e10_34-37.cpp
+++ e10_34-37.cpp
@@ -14,12 +14,13 @@
 #include <vector>
 #include <sstream>
 #include <list>
+#include <algorithm>
+#include <iterator>
 
 int main()
 {
     std::istringstream iss{"3 1 4 1 5 9 2"};
     std::vector<int> ivec;
-    std::list<int> ilist;
     int ibuf;
 
     while (iss >> ibuf) {
@@ -30,20 +31,41 @@ int main()
     for (auto i = ivec.crbegin(); i != ivec.crend(); ++i) {
         std::cout << *i << " ";
     }
-
     std::cout << std::endl;
 
     /******************** E10.35 ********************/ 
     for (auto i = ivec.cend(); i != ivec.cbegin();) {
         std::cout << *--i << " ";
     }
+    std::cout << std::endl;
 
     /************************************************/
 
-    iss.str("1 6 1 8 0 3 3 9 8 8 7");
+    iss.clear();
+    iss.str(std::string("1 6 1 8 0 3 3 9 8 8 7 4 9 8 9 4 8 4 8 2 0 4 5 8 6 8"));
+    std::list<int> ilist;
+
     while (iss >> ibuf) {
         ilist.push_back(ibuf);
     }
+    for (auto i = ilist.cbegin(); i != ilist.cend(); ++i) {
+        std::cout << *i << " ";
+    }
     
     /******************** E10.36 ********************/ 
+    auto it = find(ilist.crbegin(), ilist.crend(), 0);
+    std::cout << "\nfound : " << *it << std::endl;
+//    std::cout << "reverse iterator next : " << *(it+1) << std::endl;
+    std::cout << "distance from the crend : " << distance(it, ilist.crend()) << std::endl;
+    std::cout << "distance from the crbegin : " << distance(ilist.crbegin(), it) << std::endl;
+    std::cout << "distance from crbegin to crend : " << distance(ilist.crbegin(), ilist.crend()) << std::endl;
+    std::cout << "distance from crend to crbegin : " << distance(ilist.crend(), ilist.crbegin()) << std::endl;
+
+    /******************** E10.37 ********************/ 
+    ivec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    ilist.clear();
+    ilist.resize(5);
+    copy(ivec.cbegin() + 2, ivec.cbegin() + 7, ilist.rbegin());
+    for (auto i:ilist) std::cout << i << " ";
+    std::cout << std::endl;
 }
