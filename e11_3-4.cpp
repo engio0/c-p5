78491d28e7f4320582228e0169d9515ee26f7e8c
Changro Yoon <cryoon@deviceeng.co.kr>
2019-02-21 15:11:00 +0900

    WIP p532

--- e11_3-4.cpp
+++ e11_3-4.cpp
@@ -5,26 +5,52 @@
 // counter.
 
 
-// #include <iostream>
-// #include <map>
-// #include <set>
-// #include <string>
-// 
-// int main()
-// {
-//     std::map<std::string, size_t> word_count;
-//     std::set<std::string> exclude = {"The", "But", "And", "Or", "An", "A",
-//                                      "the", "but", "and", "or", "an", "a"};
-//     std::string word;
-// 
-//     while (std::cin >> word) {
-//         if (exclude.find(word) == exclude.end()) {
-//             ++word_count[word];
-//         }
-//     }
-// 
-//     for (const auto &w : word_count) {
-//         std::cout << w.first << " occurs " << w.second
-//                   << ( (w.second > 1) ? " times" : " time" ) << std::endl;
-//     }
-// }
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <cctype>
+
+std::string to_upper_str(std::string const & str)
+{
+    std::string upper;
+    for (auto c:str) {
+        upper.push_back(toupper(c));
+    }
+    return upper;
+}
+
+int main()
+{
+    std::map<std::string, size_t> word_count, ciword_count;
+    std::set<std::string> exclude = {"The", "But", "And", "Or", "An", "A",
+                                     "the", "but", "and", "or", "an", "a"};
+    std::string word;
+    char ch;
+
+    while (std::cin.get(ch)) {
+        if (isalnum(ch)) {
+            word.push_back(ch);
+//            std::cout << "push back : \"" << ch << "\"";
+            continue;
+        }
+        if (word.size()) {
+            ++word_count[word];
+//            std::cout << "registered : " << word << std::endl;
+            ++ciword_count[to_upper_str(word)];
+            word.clear();
+        }
+    }
+
+    for (const auto &w : word_count) {
+        std::cout << w.first << " occurs " << w.second
+                  << ( (w.second > 1) ? " times" : " time" ) << std::endl;
+    }
+    std::cout << "********** word count : " << word_count.size() << std::endl;
+
+    for (const auto &w : ciword_count) {
+        std::cout << w.first << " occurs " << w.second
+                  << ( (w.second > 1) ? " times" : " time" ) << std::endl;
+    }
+    std::cout << "********** ci word count : " << ciword_count.size() << std::endl;
+}
