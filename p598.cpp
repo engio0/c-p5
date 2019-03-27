a32dd887798b19ff26f13f1892dbcf44ae886eb1
Changro Yoon <cryoon@deviceeng.co.kr>
2019-03-27 15:31:12 +0900

    test

--- p598.cpp
+++ p598.cpp
@@ -3,9 +3,16 @@
 int main()
 {
     char arr[0];
+    std::cout << (void*)arr << std::endl;
+
     char *cp = new char[0];
 
     std::cout << (void*)cp << std::endl;
     cp = new char[5]{'H', 'e', 'l', 'l', 'o'};
     std::cout << (void*)cp << std::endl;
+
+    std::cout << cp[0] << ", " << cp[1] << std::endl;
+
+    delete[] cp;
+    std::cout << cp[0] << ", " << cp[1] << std::endl;
 }
