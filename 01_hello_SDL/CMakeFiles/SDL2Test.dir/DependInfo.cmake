fa13eda39220efec20b14274ff96c010d97bbe89
Changro Yoon <cryoon@deviceeng.co.kr>
2019-05-16 18:24:12 +0900

    WIP at work...

---
+++ 01_hello_SDL/CMakeFiles/SDL2Test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Dev/projects/sdl/01_hello_SDL/01_hello_SDL.cpp" "D:/Dev/projects/sdl/01_hello_SDL/CMakeFiles/SDL2Test.dir/01_hello_SDL.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "D:/lib/mingw/x86_64-w64-mingw32/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
