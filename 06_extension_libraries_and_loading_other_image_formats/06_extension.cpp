4d8b24460d358802cb506d7232fe7ba0ff5ed51b
Crayoon <engio@naver.com>
2019-05-27 21:33:28 +0900

    wip...

---
+++ 06_extension_libraries_and_loading_other_image_formats/06_extension.cpp
@@ -0,0 +1,6 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <windows.h>
+#include <string>
+#include <cstdio>
+
---
+++ 06_extension_libraries_and_loading_other_image_formats/CMakeFiles/SDLTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDLTest.dir/06_extension_libraries_and_loading_other_image_formats.cpp.obj"
+  "SDLTest.pdb"
+  "SDLTest.exe"
+  "SDLTest.exe.manifest"
+  "libSDLTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDLTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
