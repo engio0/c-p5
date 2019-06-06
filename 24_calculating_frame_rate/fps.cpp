5d31c7af499ee5f9dc1a02e8838728ffcd0e3089
Crayoon <engio@naver.com>
2019-06-06 14:40:20 +0900

    WIP....

---
+++ 24_calculating_frame_rate/fps.cpp
@@ -0,0 +1,293 @@
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <sstream>
+
+class LTimer
+{
+    public:
+        void start();
+        void stop();
+        void pause();
+        void unpause();
+        Uint32 getTicks();
+        bool isStarted() { return mStarted; }
+        bool isPaused() { return mPaused; }
+    private:
+        Uint32 mStartTicks = 0;
+        Uint32 mPausedTicks = 0;
+        bool mPaused = false;
+        bool mStarted = false;
+};
+
+class LTexture
+{
+    public:
+        ~LTexture() { free(); }
+        void free();
+        bool loadFromFile(std::string path);
+        bool loadFromRenderedText(std::string text, SDL_Color color);
+        int getWidth() { return mWidth; }
+        int getHeight() { return mHeight; }
+        void render(int x, int y, SDL_Rect *clip = NULL);
+    private:
+        SDL_Texture *mTexture = NULL;
+        int mWidth = 0;
+        int mHeight = 0;
+};
+
+bool init();
+void close();
+
+SDL_Window *gWindow = NULL;
+SDL_Renderer *gRenderer = NULL;
+TTF_Font *gFont = NULL;
+int SCREEN_WIDTH = 640, SCREEN_HEIGHT = 480;
+
+int main(int, char*[])
+{
+    if (!init()) {
+        printf("Error in main::init()\n");
+        return 1;
+    }
+
+    SDL_Color textColor = {0, 0, 0, 255};
+    LTimer fpsTimer;
+    LTexture fpsTextTexture;
+    std::stringstream timeText;
+    int countFrames = 0;
+    double avgFPS = 0.0;
+
+    fpsTimer.start();
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_KEYDOWN:
+                    switch (e.key.keysym.sym) {
+                        case SDLK_q:
+                            quit = true;
+                            break;
+                        default:
+                            break;
+                    }
+                default:
+                    break;
+            }
+        }
+        avgFPS = countFrames / (fpsTimer.getTicks() / 1000.0);
+
+        timeText.str("");
+        timeText << "Average Frames Per Second : "  << avgFPS;
+
+        if (!fpsTextTexture.loadFromRenderedText(timeText.str().c_str(), textColor)) {
+            printf("Unable to render FPS texture!\n");
+        }
+
+        SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
+        SDL_RenderClear(gRenderer);
+
+        fpsTextTexture.render((SCREEN_WIDTH - fpsTextTexture.getWidth())/2,
+                              (SCREEN_HEIGHT - fpsTextTexture.getHeight())/2);
+
+        SDL_RenderPresent(gRenderer);
+        timeText.str("");
+        timeText << "SDL2 Tutorial - " << countFrames << " / " << (fpsTimer.getTicks()/1000.0);
+        SDL_SetWindowTitle(gWindow, timeText.str().c_str());
+        ++countFrames;
+    }
+    close();
+    return 0;
+}
+
+
+
+void LTimer::start()
+{
+    mStarted = true;
+    mPaused = false;
+    mStartTicks = SDL_GetTicks();
+    mPausedTicks = 0;
+}
+
+void LTimer::stop()
+{
+    mStarted = false;
+    mPaused = false;
+    mStartTicks = 0;
+    mPausedTicks = 0;
+}
+
+void LTimer::pause()
+{
+    if (mStartTicks && !mPaused) {
+        mPaused = true;
+        mPausedTicks = SDL_GetTicks() - mStartTicks;
+        mStartTicks = 0;
+    }
+}
+
+void LTimer::unpause()
+{
+    if (mStarted && mPaused) {
+        mPaused = false;
+        mStartTicks = SDL_GetTicks() - mPausedTicks;
+        mPausedTicks = 0;
+    }
+}
+
+Uint32 LTimer::getTicks()
+{
+    Uint32 time = 0;
+    if (mStarted) {
+        if (mPaused) {
+            time = mPausedTicks;
+        } else {
+            time = SDL_GetTicks() - mStartTicks;
+        }
+    }
+    return time;
+}
+
+bool init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+
+#ifdef SDL_IMG_H_
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        return false;
+    }
+#endif
+
+#ifdef SDL_TTF_H_
+    if (TTF_Init()) {
+        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+        return false;
+    }
+#endif
+
+#ifdef SDL_MIX_H_
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        printf("SDL_mixer could not initialize! SDL_mixer error: %s\n", Mix_GetError());
+        return false;
+    }
+#endif
+
+    gWindow = SDL_CreateWindow("SDL2 Tutorial",
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if (!gWindow) {
+        printf("SDL_CreateWindow failed! in init\n");
+        return false;
+    }
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (!gRenderer) {
+        printf("SDL_CreateRenderer failed in init\n");
+        return false;
+    }
+    gFont = TTF_OpenFont("./lazy.ttf", 28);
+    if (!gFont) {
+        printf("TTF_OpenFont failed! in init\n");
+        return false;
+    }
+    return true;
+}
+
+void close()
+{
+//    gPromptTexture.free();
+
+//    Mix_FreeChunk(gScratch);
+//    Mix_FreeChunk(gHigh);
+//    Mix_FreeChunk(gMedium);
+//    Mix_FreeChunk(gLow);
+//
+//    Mix_FreeMusic(gMusic);
+
+    TTF_CloseFont(gFont);
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+#ifdef SDL_MIX_H_
+    Mix_Quit();
+#endif
+#ifdef SDL_IMG_H_
+    IMG_Quit();
+#endif
+    SDL_Quit();
+}
+
+
+void LTexture::free()
+{
+    if (mTexture) {
+        SDL_DestroyTexture(mTexture);
+        mTexture = NULL;
+        mWidth = 0;
+        mHeight = 0;
+    }
+}
+
+#ifdef SDL_IMG_H_
+bool LTexture::loadFromFile(std::string path)
+{
+    if (mTexture) free();
+
+    SDL_Surface *tmpSurface = IMG_Load(path.c_str());
+    if (!tmpSurface) {
+        printf("IMG_Load file %s failed! IMG Error: %s", path.c_str(), IMG_GetError());
+        return false;
+    }
+    mTexture = SDL_CreateTextureFromSurface(gRenderer, tmpSurface);
+    if (!mTexture) {
+        printf("SDL_CreateTextureFromSurface file %s failed! IMG Error: %s", path.c_str(), IMG_GetError());
+        return false;
+    }
+    mWidth = tmpSurface->w;
+    mHeight = tmpSurface->h;
+    SDL_FreeSurface(tmpSurface);
+    return true;
+}
+#endif
+
+#ifdef SDL_TTF_H_
+bool LTexture::loadFromRenderedText(std::string text, SDL_Color color)
+{
+    if (mTexture) free();
+
+    SDL_Surface *tmpSurface = TTF_RenderText_Solid(gFont, text.c_str(), color);
+    if (!tmpSurface) {
+        printf("TTF_RenderText_Solif failed!\n");
+        return false;
+    }
+    mTexture = SDL_CreateTextureFromSurface(gRenderer, tmpSurface);
+    if (!mTexture) {
+        printf("SDL_CreateTextureFromSurface failed!\n");
+        return false;
+    }
+    mWidth = tmpSurface->w;
+    mHeight = tmpSurface->h;
+
+    SDL_FreeSurface(tmpSurface);
+    return true;
+}
+#endif
+
+void LTexture::render(int x, int y, SDL_Rect *clip)
+{
+    SDL_Rect destRect = {x, y, mWidth, mHeight};
+    if (clip) {
+        destRect.w = clip->w;
+        destRect.h = clip->h;
+    }
+    SDL_RenderCopy(gRenderer, mTexture, clip, &destRect);
+}
