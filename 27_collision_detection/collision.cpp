1c47b0ede9b44204165c961492a17a6ab8d20eb5
Changro Yoon <cryoon@deviceeng.co.kr>
2019-06-10 18:04:38 +0900

    WIP...

--- 27_collision_detection/collision.cpp
+++ 27_collision_detection/collision.cpp
@@ -1,6 +1,5 @@
 #include <SDL.h>
 #include <SDL_image.h>
-#include <SDL_ttf.h>
 #include <cstdio>
 #include <string>
 
@@ -28,7 +27,6 @@ class LTexture
 class Dot
 {
     public:
-        Dot();
         void handleEvent(SDL_Event &e);
         void move(SDL_Rect &wall);
         void render();
@@ -49,11 +47,63 @@ SDL_Renderer *gRenderer = NULL;
 #ifdef SDL_TTF_H_
 TTF_Font *gFont = NULL;
 #endif
+LTexture gDotTexture;
 const int SCREEN_WIDTH = 640, SCREEN_HEIGHT = 480;
 
 
 int main(int, char*[])
 {
+    if (!init()) {
+        printf("Error in main::init!\n");
+        return 1;
+    }
+    if (!loadMedia()) {
+        printf("Error in main::loadMedia!\n");
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+
+    Dot dot;
+
+    SDL_Rect wall = {300, 40, 40, 400};
+
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_KEYDOWN:
+                    switch (e.key.keysym.sym) {
+                        case SDLK_q:
+                            quit = true;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            dot.handleEvent(e);
+        }
+        dot.move(wall);
+
+        SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
+        SDL_RenderClear(gRenderer);
+
+        SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0xff);
+        SDL_RenderDrawRect(gRenderer, &wall);
+
+        dot.render();
+
+        SDL_RenderPresent(gRenderer);
+    }
+
+    close();
+    
     return 0;
 }
 
@@ -115,3 +165,134 @@ void LTexture::render(int x, int y, SDL_Rect *clip, double angle, SDL_Point *cen
     }
     SDL_RenderCopyEx(gRenderer, mTexture, clip, &destRect, angle, center, flip);
 }
+
+void Dot::handleEvent(SDL_Event &e)
+{
+    if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
+        switch (e.key.keysym.sym) {
+            case SDLK_UP: mVelY -= DOT_VEL; break;
+            case SDLK_DOWN: mVelY += DOT_VEL; break;
+            case SDLK_LEFT: mVelX -= DOT_VEL; break;
+            case SDLK_RIGHT: mVelX += DOT_VEL; break;
+        }
+    }
+    if (e.type == SDL_KEYUP) {
+        mVelX = 0;
+        mVelY = 0;
+    }
+}
+
+void Dot::move(SDL_Rect &wall)
+{
+    mPosX += mVelX;
+    mPosY += mVelY;
+    mCollider.x = mPosX;
+    mCollider.y = mPosY;
+
+    if ((mPosX < 0) || (mPosX + DOT_WIDTH > SCREEN_WIDTH) || checkCollision(mCollider, wall)) {
+        mPosX -= mVelX;
+        mCollider.x = mPosX;
+    }
+    if ((mPosY < 0) || (mPosY + DOT_HEIGHT > SCREEN_HEIGHT) || checkCollision(mCollider, wall)) {
+        mPosY -= mVelY;
+        mCollider.y = mPosY;
+    }
+}
+
+void Dot::render()
+{
+    gDotTexture.render(mPosX, mPosY);
+}
+
+bool loadMedia()
+{
+    if (!gDotTexture.loadFromFile("./dot.bmp")) {
+        printf("Error opening dot.bmp!\n");
+        return false;
+    }
+    return true;
+}
+
+bool checkCollision(SDL_Rect &a, SDL_Rect &b)
+{
+    if ( (a.x > b.x + b.w) || (b.x > a.x + a.w) ||
+         (a.y > b.y + b.h) || (b.y > a.y + a.h) ) return false;
+    return true;
+}
+
+bool init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+
+#ifdef SDL_IMG_H_
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        return false;
+    }
+#endif
+
+#ifdef SDL_TTF_H_
+    if (TTF_Init()) {
+        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+        return false;
+    }
+#endif
+
+#ifdef SDL_MIX_H_
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        printf("SDL_mixer could not initialize! SDL_mixer error: %s\n", Mix_GetError());
+        return false;
+    }
+#endif
+
+    gWindow = SDL_CreateWindow("SDL2 Tutorial",
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if (!gWindow) {
+        printf("SDL_CreateWindow failed! in init\n");
+        return false;
+    }
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!gRenderer) {
+        printf("SDL_CreateRenderer failed in init\n");
+        return false;
+    }
+#ifdef SDL_TTF_H_
+    gFont = TTF_OpenFont("./lazy.ttf", 28);
+    if (!gFont) {
+        printf("TTF_OpenFont failed! in init\n");
+        return false;
+    }
+#endif
+    return true;
+}
+
+void close()
+{
+//    gPromptTexture.free();
+
+//    Mix_FreeChunk(gScratch);
+//    Mix_FreeChunk(gHigh);
+//    Mix_FreeChunk(gMedium);
+//    Mix_FreeChunk(gLow);
+//
+//    Mix_FreeMusic(gMusic);
+
+#ifdef SDL_TTF_H_
+    TTF_CloseFont(gFont);
+#endif
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+#ifdef SDL_MIX_H_
+    Mix_Quit();
+#endif
+#ifdef SDL_IMG_H_
+    IMG_Quit();
+#endif
+    SDL_Quit();
+}
+
