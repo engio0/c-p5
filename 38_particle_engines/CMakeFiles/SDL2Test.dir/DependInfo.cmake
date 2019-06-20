734621eb571f0c0241e55e47269f4539efba45c1
Changro Yoon <cryoon@deviceeng.co.kr>
2019-06-20 17:57:40 +0900

    WIP...

---
+++ 38_particle_engines/CMakeFiles/SDL2Test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Dev/primer/38_particle_engines/particle.cpp" "D:/Dev/primer/38_particle_engines/CMakeFiles/SDL2Test.dir/particle.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "D:/usr/x86_64-w64-mingw32/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 38_particle_engines/particle.cpp
@@ -0,0 +1,254 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <string>
+#include <cstdio>
+
+class LTexture
+{
+    public:
+        ~LTexture() { free(); }
+        void free();
+        int getWidth() { return mWidth; }
+        int getHeight() { return mHeight; }
+        bool loadFromFile(std::string path);
+#ifdef SDL_TTF_H_
+        bool loadFromRenderedText(std::string text, SDL_Color color);
+#endif
+        void setColor(Uint8 r, Uint8 g, Uint8 b) { SDL_SetTextureColorMod(mTexture, r, g, b); }
+        void setBlendMode(SDL_BlendMode blending) { SDL_SetTextureBlendMode(mTexture, blending); }
+        void setAlpha(Uint8 alpha) { SDL_SetTextureAlphaMod(mTexture, alpha); }
+        void render(int x, int y, SDL_Rect *clip = NULL, double angle = 0.0,
+                    SDL_Point *center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+    private:
+        SDL_Texture *mTexture = NULL;
+        int mWidth = 0, mHeight = 0;
+};
+
+class Dot
+{
+    public:
+        static const int DOT_WIDTH = 20, DOT_HEIGHT = 20;
+        static const int DOT_VEL = 10;
+        void handleEvent(SDL_Event &e);
+        void move();
+        void render(int camX, int camY);
+        int getPosX() { return mPosX; }
+        int getPosY() { return mPosY; }
+    private:
+        int mPosX = 0, mPosY = 0;
+        int mVelX = 0, mVelY = 0;
+};
+
+class Particle
+{
+    public:
+        Particle(int x, int y) : mPosX{x}, mPosY{y} {}
+        void render();
+        bool isDead();
+    private:
+        int mPosX = 0, mPosY = 0;
+        int mFrame = 0;
+        LTexture *mTexture;
+};
+
+SDL_Window *gWindow = NULL;
+SDL_Renderer *gRenderer = NULL;
+LTexture gDotTexture;
+const int SCREEN_WIDTH = 640, SCREEN_HEIGHT = 480;
+const int TOTAL_PARTICLES = 20;
+
+int main(int, char**)
+{
+    return 0;
+}
+
+
+void LTexture::free()
+{
+    if (mTexture) {
+        SDL_DestroyTexture(mTexture);
+        mWidth = 0;
+        mHeight = 0;
+    }
+}
+
+bool LTexture::loadFromFile(std::string path)
+{
+    free();
+    SDL_Surface *tmpSurface = IMG_Load(path.c_str());
+    if (!tmpSurface) {
+        printf("IMG_Load failed opening file %s! IMG_Error: %s\n", path.c_str(), IMG_GetError());
+        return false;
+    }
+    SDL_SetColorKey(tmpSurface, SDL_TRUE, SDL_MapRGB(tmpSurface->format, 0, 255, 255));
+    mTexture = SDL_CreateTextureFromSurface(gRenderer, tmpSurface);
+    if (!mTexture) {
+        printf("SDL_CreateTextureFromSurface failed!\n");
+        return false;
+    }
+    mWidth = tmpSurface->w;
+    mHeight = tmpSurface->h;
+    SDL_FreeSurface(tmpSurface);
+    return true;
+}
+
+#ifdef SDL_TTF_H_
+bool LTexture::loadFromRenderedText(std::string text, SDL_Color color)
+{
+    free();
+    SDL_Surface *tmpSurface =  TTF_RenderText_Solid(gFont, text.c_str(), color);
+    if (!tmpSurface) {
+        printf("TTF_RenderText_Solid failed!\n");
+        return false;
+    }
+    mTexture = SDL_CreateTextureFromSurface(gRenderer, tmpSurface);
+    if (!mTexture) {
+        printf("SDL_CreateTextureFromSurface failed rendering from text\n");
+        return false;
+    }
+    mWidth = tmpSurface->w;
+    mHeight = tmpSurface->h;
+    SDL_FreeSurface(tmpSurface);
+    return true;
+}
+#endif
+
+void LTexture::render(int x, int y, SDL_Rect *clip, double angle, SDL_Point *center, SDL_RendererFlip flip)
+{
+    SDL_Rect destRect = {x, y, mWidth, mHeight};
+    if (clip) {
+        destRect.w = clip->w;
+        destRect.h = clip->h;
+    }
+    SDL_RenderCopyEx(gRenderer, mTexture, clip, &destRect, angle, center, flip);
+}
+
+void Dot::handleEvent(SDL_Event &e)
+{
+    if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
+        switch (e.key.keysym.sym) {
+            case SDLK_UP: mVelY -= DOT_VEL; break;
+            case SDLK_DOWN: mVelY += DOT_VEL; break;
+            case SDLK_LEFT: mVelX -= DOT_VEL; break;
+            case SDLK_RIGHT: mVelX += DOT_VEL; break;
+        }
+    }
+    if (e.type == SDL_KEYUP) {
+        switch (e.key.keysym.sym) {
+            case SDLK_LEFT:
+            case SDLK_RIGHT:
+                mVelX = 0;
+                break;
+            case SDLK_UP:
+            case SDLK_DOWN:
+                mVelY = 0;
+                break;
+        }
+    }
+}
+
+void Dot::move()
+{
+    mPosX += mVelX;
+    if ( (mPosX < 0) || (mPosX + DOT_WIDTH > SCREEN_WIDTH) ) {
+        mPosX -= mVelX;
+    }
+    mPosY += mVelY;
+    if ( (mPosY < 0) || (mPosY + DOT_HEIGHT > SCREEN_HEIGHT) ) {
+        mPosY -= mVelY;
+    }
+}
+
+void Dot::render(int camX, int camY)
+{
+    gDotTexture.render(mPosX - camX, mPosY - camY);
+}
+
+bool loadMedia()
+{
+    if (!gDotTexture.loadFromFile("./dot.bmp")) {
+        printf("Error opening dot.bmp!\n");
+        return false;
+    }
+//    if (!gBackground.loadFromFile("./bg.png")) {
+//        printf("Error opening bg.bmp!\n");
+//        return false;
+//    }
+    return true;
+}
+
+bool init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+
+#ifdef SDL_IMG_H_
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        return false;
+    }
+#endif
+
+#ifdef SDL_TTF_H_
+    if (TTF_Init()) {
+        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+        return false;
+    }
+#endif
+
+#ifdef SDL_MIX_H_
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        printf("SDL_mixer could not initialize! SDL_mixer error: %s\n", Mix_GetError());
+        return false;
+    }
+#endif
+
+    gWindow = SDL_CreateWindow("SDL2 Tutorial",
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if (!gWindow) {
+        printf("SDL_CreateWindow failed! in init\n");
+        return false;
+    }
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!gRenderer) {
+        printf("SDL_CreateRenderer failed in init\n");
+        return false;
+    }
+#ifdef SDL_TTF_H_
+    gFont = TTF_OpenFont("./lazy.ttf", 28);
+    if (!gFont) {
+        printf("TTF_OpenFont failed! in init\n");
+        return false;
+    }
+#endif
+    return true;
+}
+
+void close()
+{
+//    gPromptTexture.free();
+
+//    Mix_FreeChunk(gScratch);
+//    Mix_FreeChunk(gHigh);
+//    Mix_FreeChunk(gMedium);
+//    Mix_FreeChunk(gLow);
+//
+//    Mix_FreeMusic(gMusic);
+
+#ifdef SDL_TTF_H_
+    TTF_CloseFont(gFont);
+#endif
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+#ifdef SDL_MIX_H_
+    Mix_Quit();
+#endif
+#ifdef SDL_IMG_H_
+    IMG_Quit();
+#endif
+    SDL_Quit();
+}
