2ed02f7b4dbbad1b94abbdb4f07d51f986cdd9f2
Changro Yoon <cryoon@deviceeng.co.kr>
2019-08-02 15:27:24 +0900

    WIP...

---
+++ wxWidgets/zetcode/firstprograms/4th/Communicate.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <wx/wx.h>
+#include "Panels.h"
+
+class Communicate:public wxFrame
+{
+public:
+    Communicate(const wxString &title);
+
+    LeftPanel *m_lp;
+    RightPanel *m_rp;
+    wxPanel *m_parent;
+};
