6adaefe6a6fa81b6da67f72ac295ef405c4f7b1a
Changro Yoon <cryoon@deviceeng.co.kr>
2019-08-02 18:01:24 +0900

    WIP...

--- wxWidgets/zetcode/menustoolbars/1st/menu.cpp
+++ wxWidgets/zetcode/menustoolbars/1st/menu.cpp
@@ -4,4 +4,16 @@ SimpleMenu::SimpleMenu(const wxString &title)
     : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(280,180))
 {
     menubar = new wxMenuBar;
+    file = new wxMenu;
+    file->Append(wxID_EXIT, wxString("&Quit"));
+    menubar->Append(file, wxT("&File"));
+    SetMenuBar(menubar);
+
+    Connect(wxID_EXIT, wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(SimpleMenu::OnQuit));
+    Center();
+}
+
+void SimpleMenu::OnQuit(wxCommandEvent &WXUNUSED(event))
+{
+    Close(true);
 }
