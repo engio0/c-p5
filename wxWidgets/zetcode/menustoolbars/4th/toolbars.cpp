3e6ff655709041dca66f4502ace4a88f643312fd
Crayoon <engio@naver.com>
2019-08-05 21:29:40 +0900

    WIP...

--- wxWidgets/zetcode/menustoolbars/4th/toolbars.cpp
+++ wxWidgets/zetcode/menustoolbars/4th/toolbars.cpp
@@ -1,6 +1,13 @@
 #include "toolbars.h"
+#include <wx/image.h>
 
 Toolbar::Toolbar(const wxString &title)
     : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(320,250))
 {
+    wxImage::AddHandler(new wxPNGHandler);
+
+    wxBitmap exit(wxT("./exit.png"), wxBITMAP_TYPE_PNG);
+    wxBitmap newb(wxT("./file_yellow_empty_new.png"));
+    wxBitmap open(wxT("./file_yellow_open.png"));
+    wxBitmap save(wxT("./file_yellow_save.png"));
 }
