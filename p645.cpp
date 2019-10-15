6739f187bd3878e856cb7411782437ae1bdd014f
Changro Yoon <cryoon@deviceeng.co.kr>
2019-10-15 18:02:38 +0900

    WIP...

--- p645.cpp
+++ p645.cpp
@@ -3,19 +3,22 @@
 #include <set>
 
 class Message {
-private:
     friend class Folder;
-    std::string contents;
-    std::set<Folder*> folders;
-    void add_to_Folders(const Message&);
-    void remove_from_Folders();
+
 public:
     explicit Message(const std::string &str = ""):contents(str) {}
     Message(const Message&);
-    Message& operator=(const Message&);
     ~Message();
+    Message& operator=(const Message&);
     void save(Folder&);
     void remove(Folder&);
+
+private:
+    std::string contents;
+    std::set<Folder*> folders;
+
+    void add_to_Folders(const Message&);
+    void remove_from_Folders();
 };
 
 class Folder {
