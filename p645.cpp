c9331fe0e51fefd83a3ee69661515df004024b2f
Changro Yoon <cryoon@deviceeng.co.kr>
2019-10-16 14:49:05 +0900

    WIP...

--- p645.cpp
+++ p645.cpp
@@ -22,9 +22,16 @@ private:
 };
 
 class Folder {
+    friend class Message;
+
 public:
+    explicit Folder(const std::string &str = "") : title(str) {}
     void addMsg(Message*);
     void remMsg(Message*);
+
+private:
+    std::string title;
+    std::set<Message*> messages;
 };
 
 
@@ -38,4 +45,36 @@ void Message::remove(Folder &f)
 {
     folders.erase(&f);
     f.remMsg(this);
+}
+
+void Message::add_to_Folders(const Message& msg)
+{
+    for (auto f : msg.folders)
+        f->addMsg(this);
+}
+
+Message::Message(const Message& msg):
+    contents(msg.contents), folders(msg.folders)
+{
+    add_to_Folders(msg);
+}
+
+void Message::remove_from_Folders()
+{
+    for (auto f : folders)
+        f->remMsg(this);
+}
+
+Message::~Message()
+{
+    remove_from_Folders();
+}
+
+Message& Message::operator=(const Message& rhs)
+{
+    remove_from_Folders();
+    contents = rhs.contents;
+    folders = rhs.folders;
+    add_to_Folders(rhs);
+    return *this;
 }
